d0645741c6821dbb4ba4320f2a3f9c0417500fcb
wightkid <103604852+wightkid@users.noreply.github.com>
2024-11-08 11:50:10 -0500

    Add univeral collision for tiles with canCollide parameter set to true

--- callaway-brandon-a3-2dgame/Player.cs
+++ callaway-brandon-a3-2dgame/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using System.Numerics;
+using System.Threading;
 
 namespace Game10003;
 
@@ -18,29 +20,41 @@ public class Player
     int playerMaxSpeed = 1000;
     int gravityForce = 10;
     float friction = 50f;
+    bool canPlayerLeft = true;
+    bool canPlayerRight = true;
+    float collisionOffset = 5;
+    float jumpForce = 500;
 
 
     // Animation variables
     Texture2D[] idleFrames;
     Texture2D[] runRightFrames;
     Texture2D[] runLeftFrames;
+    Texture2D[] danceFrames;
+    Texture2D[] jumpFrames;
     int currentFrame = 0;
     float animationSpeed = 10f;
     float currentFloatFrame = 0;
+    bool isPlayerDancing = false;
+    bool isPlayerJumping = false;
 
     public Player()
     {
-        // Initialize animation frames
+        // Declare animation frame arrays
         idleFrames = new Texture2D[5];
         runRightFrames = new Texture2D[5];
         runLeftFrames = new Texture2D[5];
+        danceFrames = new Texture2D[5];
+        jumpFrames = new Texture2D[5];
 
+        // Load individual frames into their respective arrays
         for (int frame = 0; frame < idleFrames.Length; frame++)
         {
-            //idleFrames[frame] = new Texture2D();
             idleFrames[frame] = Graphics.LoadTexture($"../../../assets/textures/playerSprites/idle/idleFrame{frame + 1}.png");
             runRightFrames[frame] = Graphics.LoadTexture($"../../../assets/textures/playerSprites/runRight/runFrame{frame + 1}.png");
             runLeftFrames[frame] = Graphics.LoadTexture($"../../../assets/textures/playerSprites/runLeft/runFrame{frame + 1}.png");
+            danceFrames[frame] = Graphics.LoadTexture($"../../../assets/textures/playerSprites/dance/danceFrame{frame + 1}.png");
+            jumpFrames[frame] = Graphics.LoadTexture($"../../../assets/textures/playerSprites/jump/jumpFrame{frame + 1}.png");
         }
     }
 
@@ -53,22 +67,34 @@ public class Player
     void HandleInput()
     {
 
-        if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+        if (Input.IsKeyboardKeyDown(KeyboardInput.F))
+        {
+            isPlayerDancing = true;
+        }
+        else
+        {
+            isPlayerDancing = false;
+        }
+
+        if (Input.IsKeyboardKeyDown(KeyboardInput.A) && canPlayerLeft)
         {
             velocity.X = -playerSpeed;
 
         }
-        else if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+        else if (Input.IsKeyboardKeyDown(KeyboardInput.D) && canPlayerRight)
         {
             velocity.X = playerSpeed;
         }
 
-        if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+        // If player can jump, allow jump. If player is already jumping, dont allow another jump
+        if (Input.IsKeyboardKeyPressed(KeyboardInput.Space) && !isPlayerJumping)
         {
-            velocity.Y -= velocity.Y + playerSpeed;
+            velocity.Y -= velocity.Y + jumpForce;
+            currentFloatFrame = 0;
+            isPlayerJumping = true;
         }
 
-        //velocity.Y += gravityForce * Time.DeltaTime * 100;
+        velocity.Y += gravityForce * Time.DeltaTime * 100;
         
         position += velocity * Time.DeltaTime;
 
@@ -84,68 +110,64 @@ public class Player
         }
     }
 
-    // TODO: FIX COLLISION
+    // Handle player collision via ch
     public void HandleCollision(Tile tile)
     {
-        //Vector2 lastPosition = position;
-        /*
-        if (tile.position.X >= position.X && tile.position.X <= position.X + playerSize && tile.position.Y >= position.Y && tile.position.Y <= position.Y + playerSize)
+        Vector2 playerTop = new Vector2(position.X + (playerSize / 2), position.Y - collisionOffset);
+        Vector2 playerBottom = new Vector2(position.X + (playerSize / 2), position.Y + playerSize - collisionOffset);
+        Vector2 playerLeft = new Vector2(position.X, position.Y + (playerSize / 2) - 10);
+        Vector2 playerRight = new Vector2(position.X + playerSize, position.Y + (playerSize / 2) - collisionOffset);
+        
+        Vector2 tileTopLeft = new Vector2(tile.position.X - (collisionOffset / 2), tile.position.Y - (collisionOffset/2));
+        Vector2 tileTopRight = new Vector2(tile.position.X + tile.size + (collisionOffset / 2), tile.position.Y - (collisionOffset / 2));
+        Vector2 tileBottomLeft = new Vector2(tile.position.X - (collisionOffset / 2), tile.position.Y + tile.size + (collisionOffset / 2));
+        Vector2 tileBottomRight = new Vector2(tile.position.X + tile.size + (collisionOffset / 2), tile.position.Y + tile.size + (collisionOffset / 2));
+
+        // Handle player left collision
+        if (playerLeft.X < tileTopRight.X && playerLeft.X > tileTopLeft.X)
         {
-            velocity = new Vector2(0, 0);
-        }
-        else if (tile.position.X >= position.X && tile.position.X <= position.X + playerSize && tile.position.Y + tile.size >= position.Y && tile.position.Y + tile.size <= position.Y + playerSize)
-        {
-            velocity = new Vector2(0, 0);
-        }
-        else if (tile.position.X + tile.size >= position.X && tile.position.X + tile.size <= position.X + playerSize && tile.position.Y >= position.Y && tile.position.Y <= position.Y + playerSize)
-        {
-            velocity = new Vector2(0, 0);
-        }
-        else if (tile.position.X + tile.size >= position.X && tile.position.X + tile.size <= position.X + playerSize && tile.position.Y + tile.size >= position.Y && tile.position.Y + tile.size <= position.Y + playerSize)
-        {
-            velocity = new Vector2(0, 0);
-        }
-        */
-
-        Vector2 tilePosition = tile.position;
-        float tileSize = tile.size;
-
-        float tileEdgeLeft = tilePosition.X;
-        float tileEdgeRight = tilePosition.X + tileSize;
-        float tileEdgeTop = tilePosition.Y;
-        float tileEdgeBottom = tilePosition.Y + tileSize;
-
-        float playerEdgeLeft = position.X -2;
-        float playerEdgeRight = position.X + playerSize;
-        float playerEdgeTop = position.Y;
-        float playerEdgeBottom = position.Y + playerSize;
-
-        bool doesCollideLeft = playerEdgeLeft < tileEdgeRight;
-        bool doesCollideRight = playerEdgeRight > tileEdgeLeft;
-        bool doesCollideTop = playerEdgeTop < tileEdgeBottom + 5;
-        bool doesCollideBottom = playerEdgeBottom > tileEdgeTop;
-
-        bool isColliding = doesCollideLeft && doesCollideRight && doesCollideTop && doesCollideBottom && tile.canCollide;
-
-
-        if (tile.canCollide && isColliding)
-        {
-
-
-            if (doesCollideRight && velocity.X > 0)
+            if (playerLeft.Y < tileBottomRight.Y && playerLeft.Y > tileTopRight.Y)
             {
-                position.X = tile.position.X - playerSize;
+                velocity.X = 0;
+                position.X = tileTopRight.X;
+                Console.WriteLine("CHECK");
             }
-            else if (doesCollideLeft && velocity.X < 0)
-            {
-                position.X = tile.position.X + tileSize;
-            }
-
         }
 
-        //Console.WriteLine(velocity.X);
+        // Handle player right collision
+        if (playerRight.X > tileTopLeft.X && playerRight.X < tileTopRight.X)
+        {
+            if (playerRight.Y < tileBottomLeft.Y && playerRight.Y > tileTopLeft.Y)
+            {
+                velocity.X = 0;
+                position.X = tileTopLeft.X - playerSize;
+                Console.WriteLine("CHECK");
+            }
+        }
+
+        // Handle player ceiling collision
+        if (playerTop.Y < tileBottomLeft.Y && playerTop.Y > tileTopLeft.Y)
+        {
+            if (playerTop.X > tileTopLeft.X && playerTop.X < tileTopRight.X)
+            {
+                velocity.Y = 0;
+                position.Y = tileBottomLeft.Y + 5;
+            }
+        }
+
+        // Handle player floor collision
+        if (playerBottom.Y > tileTopLeft.Y && playerBottom.Y < tileBottomLeft.Y)
+        {
+            if (playerBottom.X > tileTopLeft.X && playerBottom.X < tileTopRight.X)
+            {
+                velocity.Y = 0;
+                position.Y = tileTopLeft.Y - playerSize + 5;
+            }
+        }
+
     }
 
+    // Render and Handle all animation frames
     void Render()
     {
         // Update currentFloatFrame by delta time * scalar to allow for frame gaps
@@ -180,22 +202,32 @@ public class Player
         //Console.WriteLine(currentFloatFrame);
 
         // If the player is moving, display run animation. Otherwise, display idle animation
-        if (velocity.X == 0)
+        if (velocity.X == 0 && !isPlayerDancing && !isPlayerJumping)
         {
             Graphics.Draw(idleFrames[currentFrame], position);
         }
-        else if (velocity.X > 0)
+        else if (velocity.X > 0 && !isPlayerDancing && !isPlayerJumping)
         {
             Graphics.Draw(runRightFrames[currentFrame], position);
         }
-        else if (velocity.X < 0)
+        else if (velocity.X < 0 && !isPlayerDancing && !isPlayerJumping)
         {
             Graphics.Draw(runLeftFrames[currentFrame], position);
         }
-            
-        
 
 
-        //Draw.Square(position, playerSize);
+        if (isPlayerJumping && !isPlayerDancing)
+        {
+            Graphics.Draw(jumpFrames[currentFrame], position);
+            if (currentFrame == 4)
+            {
+                isPlayerJumping = false;
+            }
+        }
+
+        if (isPlayerDancing && !isPlayerJumping)
+        {
+            Graphics.Draw(danceFrames[currentFrame], position);
+        }
     }
 }
